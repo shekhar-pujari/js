650ae25c24f0536501d27be5c2e9cef9d3079d01
shekhar-pujari <127793295+shekhar-pujari@users.noreply.github.com>
2023-12-29 13:02:19 +0000

    Data types summary

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,47 @@
+//let score = 33
+// let score = "33"
+// let score = null
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let value_num = Number(score)
+
+// console.log(typeof value_num)
+// console.log(typeof(value_num))
+
+/* 
+to number
+"33" > 33
+"33abc" > NaN
+ture > 1 false > 0
+*/
+
+/*
+to boolean
+1 > true; 0 > false
+"" > false
+"sjkd" > true
+*/
+
+// ****** Operations ******
+
+let val = 3
+let negVal = -val 
+// console.log(negVal)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+// let txt = "Hello"
+// let txt1 = " how are you"
+
+// let txt3 = txt + txt1
+// console.log(txt3)
+
+// let gameCounter = 100
+// gameCounter++
---
+++ 01_Basics/05_DataTypesSummary.js
@@ -0,0 +1,56 @@
+/*
+Data types divided into two types
+
+1. Premitive (Call by value)
+    String, Number, Boolean, null, undefined, Symbol, BigInt
+
+2. Non-Primitive (Referece Type). Always return object
+    Object, Array, Functions
+
+Javascript dynamically typed language
+*/
+
+const score = 100
+const score_val = 83.65
+const isLogged = false 
+const temp = null
+let userEmail
+
+const id = Symbol("123")
+const another_id = Symbol("123")
+
+console.log(id === another_id)
+
+const big_num = 556658554455n
+
+//
+const heros = ["Akshay", "Amir", "Salman", "Hritesh"]
+
+let obj = {
+        name: "Akshay",
+        age: 40
+}
+
+const myFunction = function(){
+    console.log("Hello From Function")
+}
+console.log(typeof obj);
+console.log(typeof heros);
+console.log(typeof myFunction);
+
+/*
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+*/
