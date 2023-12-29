d55f2d855cefa92dfbf7e7cbba0ef46624543372
shekhar-pujari <127793295+shekhar-pujari@users.noreply.github.com>
2023-12-29 15:31:15 +0000

    Arrays part 2

---
+++ 02_Basics/02_Arrays.js
@@ -0,0 +1,27 @@
+const class1 = ["Mohit", "Dinesh", "Rajesh"]
+const class2 = ["Sony", "Rocky", "Mickey"]
+
+// class1.push(class2)
+// console.log(class1);
+
+// const combined = class1.concat(class2)
+// console.log(combined);
+
+// const combined = [...class1, ...class2]
+// console.log(combined);
+
+// const arr = [1,2,3, [4,5,6], 7, [6,7,[4,5]]]
+// const another = arr.flat(Infinity)
+// console.log(another);
+
+// console.log(Array.isArray("Rohan"));
+// console.log(Array.from("Rohan"));
+// console.log(Array.from({name:"Sheetal"})); // returns empty array, will be discussed later
+
+const s1 = 100
+const s2 = 200
+const s3 = 200
+
+console.log(Array.of(s1,s2,s3));
+
+// 15 Complete
