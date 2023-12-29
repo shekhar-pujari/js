6d3db40ce9512cdd51cc658db24244c71242b358
shekhar-pujari <127793295+shekhar-pujari@users.noreply.github.com>
2023-12-29 15:31:07 +0000

    Arrays part 1

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,33 @@
+// Arrays
+
+const arr = [0, 1, 2, 3, 4, 5]
+const names = ["Salman", "Mohit", "Rakesh"]
+const cities = new Array("Pune", "Bangalore", "Chennai", "Mysore", "Goa")
+
+// console.log(arr);
+// console.log(names);
+// console.log(cities);
+
+// Methods
+// cities.push("Nagpur")
+// console.log(cities);
+
+// cities.pop()
+// console.log(cities);
+
+// cities.unshift("Mysore") // adds at very first position
+// console.log(cities);
+
+// cities.shift() // removed from first position
+// console.log(cities);
+
+// console.log(cities.includes("pune")); // Case sensitive
+// console.log(cities.indexOf("Chennai"));
+
+// console.log(cities.join());
+
+// Slice, Splice
+
+console.log("A ", cities);
+console.log(cities.slice(1,3));
+console.log(cities.splice(1,3));
